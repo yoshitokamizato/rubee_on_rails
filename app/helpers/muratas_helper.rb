dd787a1e003a08f1c274f8ead666e0f51d3efafc
村田彩 <murataaya@murataayanoMacBook-Pro.local>
2018-10-13 14:56:56 +0900

    add murata page

---
+++ app/helpers/muratas_helper.rb
@@ -0,0 +1,2 @@
+module MuratasHelper
+end
---
+++ test/controllers/muratas_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MuratasControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
